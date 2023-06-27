f9b64b6987f08c0811bc38ce942d96ef20eba032
Anthony <anthony.schafrik@e-hps.com>
2023-06-27 14:36:02 -0500

    Added in base map

---
+++ map.rb
@@ -0,0 +1,6 @@
+module Map
+    Town = ['Center']
+    Mountain = ['Mountain Trail', 'Mountain Base', 'Mountain Side', 'Mountain Peak']
+    Volcano = ['Volcano Trail', 'Volcano Base', 'Volcano Side', 'Volcano Peak']
+    River = ['River Trail', 'River Base', 'River Side', 'River Peak']
+end
